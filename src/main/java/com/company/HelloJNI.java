3ebf02b5bd409fb5ad6de8b999222136c2a86251
吕途 <buzhidaolvtu@163.com>
2017-11-10 17:08:40 +0800

    java execute javascript

--- src/main/java/com/company/HelloJNI.java
+++ src/main/java/com/company/HelloJNI.java
@@ -4,19 +4,6 @@ import java.util.*;
 
 public class HelloJNI {
 
-    private static native void hehe();
-
-    // Declare a native method sayHello() that receives nothing and returns void
-    private native void sayHello();
-
-    private native void sayHello(String hello);
-
-    private native String executeScript(String script);
-
-    public static void hello2() {
-        System.out.println("hello2");
-    }
-
     private static Set<Object> referenceCount = new HashSet();
 
     public static JConnection createConn(String url) {
@@ -28,7 +15,7 @@ public class HelloJNI {
 
 
     public static void main(String[] args) {
-        V8.initialize("/Users/lvtu/Library/Developer/Xcode/DerivedData/v8-hello-world-cwwhrrfzjyeesxhchgyxuueyceek/Build/Products/Debug/");
+        V8.initialize(System.getProperty("java.library.path"));
         JSContext context = new JSContext();
         context.initialize();
         String s = context.executeScript(
